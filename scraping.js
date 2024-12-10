d0e6e71a186d46012abe50c6cc21949ed98497a0
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-12-10 10:14:39 +0900

    node.js記事のいいねソート画面から記事を取得するところまで実装

---
+++ scraping.js
@@ -0,0 +1,50 @@
+// クロニウムを使って、Zennにアクセスする
+import { chromium } from "@playwright/test";
+
+import { scrapeArticlesWithHighLikes } from "./scrapeArticlesWithHighLikes.js";
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 300,
+  });
+  
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  
+  await page.goto("https://zenn.dev");
+
+  const searchInput = page.locator("#header-search");
+  await searchInput.click();
+
+  // 部分的なクラス名とhrefの組み合わせ
+  // TODO: 配列で複数実行できるようにしたい
+  const topicLink = page.locator(
+    'a[class*="TopicCardList_link"][href="/topics/nodejs"]'
+  );
+  await topicLink.click();
+
+  const allTimeSortButton = page.locator('a[class*="TabLinkGroup_link"]', {
+    hasText: 'Alltime'
+  });
+  await allTimeSortButton.click();
+
+  const articles = await scrapeArticlesWithHighLikes(page);
+  console.log(articles);
+
+  // await browser.close();
+})();
+
+// import { chromium } from "@playwright/test";
+
+// (async () => {
+//   // @see https://playwright.dev/docs/api/class-browsertype#browser-type-launch
+//   const browser = await chromium.launch({ headless: false, slowMo: 500 });
+//   const page = await browser.newPage();
+//   await page.goto("http://localhost:3000");
+//   const htmlStr = await page.content();
+
+//   console.log(htmlStr);
+
+//   await browser.close();
+// })();
